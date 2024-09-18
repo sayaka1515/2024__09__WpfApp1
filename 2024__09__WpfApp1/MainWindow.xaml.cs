b8c017f053d9fd397ed3245cceb2df00b0537c50
英豪 李 <5b1g0009@office.stust.edu.tw>
2024-09-18 11:07:45 +0800

    新增了能偵測某個數值是否大於2亦或是小於2的效果

--- 2024__09__WpfApp1/MainWindow.xaml.cs
+++ 2024__09__WpfApp1/MainWindow.xaml.cs
@@ -23,8 +23,28 @@ namespace _2024__09__WpfApp1
 
 
         private void MyButton_Click(object sender,RoutedEventArgs e)
-        { 
-            MessageBox.Show("Hello World");
+        {
+            //MessageBox.Show("Hello World");
+
+            int number;
+            bool success = int.TryParse(MyTextBox.Text, out number) ;
+            if (!success)
+            {
+                MessageBox.Show("請輸入整數", "輸入錯誤");
+            }
+            else if (number < 2)
+            {
+                MessageBox.Show($"輸入的數值為{number}, 小於2，請重新輸入", "輸入錯誤");
+
+            }
+            else //(number >= 2)
+            {
+                MessageBox.Show($"輸入的數值為{number}, 可以進一步執行", "輸入正確");
+                /*for (int i = 2; i < number; i++) 
+                if (IsPrime(i)){
+                
+                }*/
+            }
         }
     }
 }
