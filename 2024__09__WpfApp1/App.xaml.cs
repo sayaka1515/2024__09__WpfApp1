bc9c42100599403383528bbe646429176055714f
英豪 李 <5b1g0009@office.stust.edu.tw>
2024-09-18 09:27:46 +0800

    加入專案檔案。

---
+++ 2024__09__WpfApp1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _2024__09__WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
